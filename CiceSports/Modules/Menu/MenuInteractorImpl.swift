3e167d5cd5b103907b0487588415cc1d5aa2690d
Adrian Sevilla <adrian.sevilla.diaz@gmail.com>
2021-06-03 16:43:40 +0200

    [WIP] Menu Interactor changed

--- CiceSports/Modules/Menu/MenuInteractorImpl.swift
+++ CiceSports/Modules/Menu/MenuInteractorImpl.swift
@@ -52,4 +52,36 @@ extension MenuInteractorImpl: MenuInteractorProtocol {
             }
         }
     }
+    
+    /*
+     func fetchDataFromHerokuBusiness(success: @escaping ([ConsejosGenerale]?) -> (), failure: @escaping (NetworkingError?) -> ()) {
+             CoreDataStack.shared.isFirsTime { (result) in
+                 if result {
+                     CoreDataStack.shared.setValueFirstTime(value: false)
+                     self.transformData(success: success, failure: failure)
+                 } else {
+                     CoreDataStack.shared.loadDataIfNeeded { (isRefreshingRequired) in
+                         if isRefreshingRequired {
+                             self.transformData(success: success, failure: failure)
+                         } else {
+                             success(CoreDataStack.shared.getConsejos())
+                         }
+                     }
+                 }
+             }
+         }
+         
+         private func transformData(success: @escaping ([ConsejosGenerale]?) -> (), failure: @escaping (NetworkingError?) -> ()) {
+             self.provider.fetchConsejos { [weak self] (result) in
+                 guard self != nil else { return }
+                 switch result{
+                 case .success(let respose):
+                     CoreDataStack.shared.setConsejos(data: respose.icoResponseConsejos?.consejosGenerales ?? [])
+                     success(CoreDataStack.shared.getConsejos())
+                 case .failure(let error):
+                     failure(error)
+                 }
+             }
+         }
+     */
 }
